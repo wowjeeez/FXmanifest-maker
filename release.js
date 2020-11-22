4ca906e9592cf0578889437f68a813409135d6f6
LedAndris <ledererandras2004@gmail.com>
2020-11-22 23:00:31 +0100

    Please

--- release.js
+++ release.js
@@ -42,17 +42,19 @@ exec(`git tag v${newV}`, (error, stdout, stderr) => {
         return;
     }
     console.log(`stdout: ${stdout}`);
+    exec(`git push`, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    })
 })
-exec(`git push`, (error, stdout, stderr) => {
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-    console.log(`stdout: ${stdout}`);
-})
+
+
 
 console.log("Pushed new release")
